f6b97797b3ed04d342e4d8db0027d32f96cfe78e
AIST-DPRT <aist-dprt@ddhcp219080.d.aist.go.jp>
2019-02-13 12:03:10 +0900

    reorgnization Jiang

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-test readme
+KDD 2019, paper submission
+DeepUrbanEvent: A System for Predicting Citywide Crowd Dynamics at Big Events
--- common/datastructure/Point.py
+++
@@ -1,9 +0,0 @@
-
-
-class Point(object):
-    def __init__(self, lon, lat):
-        self.lon = lon
-        self.lat = lat
-
-    def toString(self):
-        return '(' + self.lon + ',' + self.lat + ')'
